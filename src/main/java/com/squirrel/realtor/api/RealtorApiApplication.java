5d1d80b630991c185e3aa88293ff33a7b56d22bb
= <vvt100020@gmail.com>
2019-02-14 00:25:19 -0600

    jdjsdssstest

--- src/main/java/com/squirrel/realtor/api/RealtorApiApplication.java
+++ src/main/java/com/squirrel/realtor/api/RealtorApiApplication.java
@@ -7,5 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RealtorApiApplication {
     public static void main(String[] args) {
             SpringApplication.run(RealtorApiApplication.class, args);
+            //comment
     }
 }
